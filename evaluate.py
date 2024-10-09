f2f69d89098c6e880601e8ea3aa08cb43b3c1381
Luisa Montanari <montanariluisa@protonmail.com>
2024-10-09 10:57:32 +0200

    test commit

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,6 @@
 from decision_tree import *
 
+
 def evaluate(model, dataset) :
     dataset = model.classify_dataset(dataset)
     df = dataset.apply(lambda dp : int(dp['variety'] == dp['classification']), axis=1)
