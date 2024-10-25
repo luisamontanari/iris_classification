8bbaa9028504a18dd91a34edccd0adb2559cebe9
Luisa Montanari <montanariluisa@protonmail.com>
2024-10-26 00:07:41 +0200

    fully functional naive bayes classifier

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,5 @@
 from decision_tree import *
 
-
 def evaluate_model(model, dataset) :
     dataset = model.classify_dataset(dataset)
     df = dataset.apply(lambda dp : int(dp['variety'] == dp['classification']), axis=1)
--- naive_bayes.py
+++ naive_bayes.py
@@ -3,16 +3,36 @@ import math
 import numpy as np
 
 class naive_bayes_model:
-    def __init__(self, data):
-        pass
+    # Naive Bayes assumption: all features are independent
+    # assumption is rarely true, but NB is still surprisingly accurate in classification tasks (but not regression!)
+    def __init__(self, training_data):
+        variance_mat, means_mat, priors_vec, class_mapping = self._build_naive_bayes_model(training_data)
+        
+        # shape of all matrices: (#classes, #features), i.e. (3, 4)
+        self.variance_mat = variance_mat.astype(float)
+        self.means_mat = means_mat.astype(float)
+        self.priors = priors_vec.astype(float)
+        self.class_mapping = class_mapping # map class indices to labels
+        return
     
     # classify a single datapoint
     def classify_datapoint(self, datapoint):
-        pass
+        dp = datapoint.drop(labels=['variety']).to_numpy().flatten().astype(float)
+        
+        # P(x(i) | y), likelihood of our observation of feature x(i) (columns) assuming class x is present (rows)
+        likelihood_mat = 1/np.sqrt(2*np.pi*self.variance_mat) * np.exp(-((dp-self.means_mat)**2) / (2*self.variance_mat)) # Gaussian PDF
+        
+        # prediction(X) 
+        # = argmax_y P(y | X)
+        # = argmax_y P(y) * prod([ P(x(i) | y) for each i in n ])
+        class_probabilities = self.priors * np.prod(likelihood_mat, axis=1)
+        prediction = self.class_mapping.get(np.argmax(class_probabilities))
+        return prediction
     
     # classify a set of datapoints
     def classify_dataset(self, df) :
-        pass
+        df.loc[:, 'classification'] = df.apply(self.classify_datapoint, axis=1, result_type='expand')
+        return df
 
 
 # Naive Bayes Assumption: 
@@ -38,39 +58,31 @@ class naive_bayes_model:
 # mean_y_i -> empirical mean of x(i) associated with class y
 # = sum(x_y_i)/count(x_y_i) where x_y_i = set of values for x(i) when restricted to class y
 
-def bayes(df) : 
-    # -------------------- model building: get priors, mean and variance matrix
-    # s.l  s.w  p.l  p.w
-    # 5.1, 3.5, 1.4, .2
-    # 4.9, 3,   1.4, .2
-    # 4.7, 3.2, 1.3, .2
-    for variety in df['variety'].unique(): # this too can be vectorized -> get mean/variance matrices instead of vectors
-        # segment dataframe by variety and convert to numpy array
-        df_by_variety = df.loc[df['variety'] == variety]
-        obs = df_by_variety.drop('variety', axis=1).to_numpy()
-        n = obs.shape[0]
-
-        # calc sample mean, variance and prior
-        mean_arr = np.mean(obs, axis=0)
-        prior = obs.shape[0]/df.shape[0]
-
-        #print(obs)
-        #print(mean_arr)
-        #print(np.sum((obs-mean_arr)**2, axis=0))
-        print('_____')
-        if (n == 1): continue # TODO: implement actual try catch block
-        # calc Bessel-corrected sample variance
-        variance_arr = 1.0/(n-1) * np.sum((obs-mean_arr)**2, axis=0)
-
-        # ----------- i think actual model building is done here, from now we assume there is an observation
+    def _build_naive_bayes_model(self, df) : 
+        # model building: get priors, mean and variance matrix
+        feature_count = df.shape[1]-1
+        class_count = df['variety'].unique().shape[0]
+        variance_mat = np.zeros((class_count, feature_count))
+        means_mat = np.zeros((class_count, feature_count))
+        priors_vec = np.zeros((class_count,))
+        class_mapping = dict() # to keep track of which class label is associated with which row index
         
-        ## P(x(i) | y) 
-        # = 1/np.sqrt(2*np.pi*variance_y_i)*np.exp(-(x(i)-mean_y_i)**2/2*variance)
-        likelihood_arr = 1/np.sqrt(2*np.pi*variance_arr) * np.exp(-(obs-mean_arr)**2/2*variance_arr) # TODO: I think something is wrong here...
-        print(likelihood_arr)
+        for i, variety in enumerate(df['variety'].unique()):
+            class_mapping.update({i:variety})
+            # segment dataframe by variety and convert to numpy array
+            df_by_variety = df.loc[df['variety'] == variety]
+            obs = df_by_variety.drop('variety', axis=1).to_numpy()
+            n = obs.shape[0]
 
-        # prediction(X) 
-        # = argmax_y P(y | X)
-        # = argmax_y P(y) * prod([ P(x(i) | y) for each i in n ])
-
-    return
+            # calc sample class mean, variance and prior
+            mean_arr = np.mean(obs, axis=0)
+            means_mat[i,:] = mean_arr
+            
+            prior = obs.shape[0]/df.shape[0] # relative frequency in dataset
+            priors_vec[i] = prior
+            
+            if (n == 1): continue # TODO: implement actual try catch block
+            # calc Bessel-corrected sample class variance
+            variance_arr = 1.0/(n-1) * np.sum((obs-mean_arr)**2, axis=0)
+            variance_mat[i,:] = variance_arr
+        return variance_mat, means_mat, priors_vec, class_mapping
