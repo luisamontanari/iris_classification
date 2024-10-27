bbb0d0f09201dad1d2e396297a5425a28bcec906
Luisa Montanari <montanariluisa@protonmail.com>
2024-10-27 15:00:05 +0100

    fix df overwrite on evaluate bug

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,8 @@
 from decision_tree import *
 
 def evaluate_model(model, dataset) :
-    dataset = model.classify_dataset(dataset)
-    df = dataset.apply(lambda dp : int(dp['variety'] == dp['classification']), axis=1)
-    accuracy = df.sum()/df.count()
+    prediction = model.classify_dataset(dataset)
+    merged_df = pd.concat([dataset, prediction], axis=1)
+    correct_classifications = merged_df.apply(lambda dp : int(dp['variety'] == dp['prediction']), axis=1)
+    accuracy = correct_classifications.sum()/dataset.shape[0]
     return accuracy
